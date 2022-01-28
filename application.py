0b21464e5d064f808d162bfb3768c0a0e29333a2
MarilynWaldman <mdwaldman22@gmail.com>
2022-01-28 07:21:37 -0700

    added weather map

--- application.py
+++ application.py
@@ -2,10 +2,13 @@ from functools import wraps, update_wrapper
 import os as os
 import datetime as dt
 from datetime import datetime
-#from xmlrpc.client import APPLICATION_ERROR
 from flask import Flask, render_template, render_template_string, redirect, send_file, make_response
-from flask import jsonify
-from make_map import make_map 
+#from flask import jsonify
+#from make_map import make_map 
+from weather_data import  get_weather_data
+from weather_map import make_weather_map 
+#import geopandas as gpd
+#import pandas as pd
 
 application = Flask(__name__)
 
@@ -37,23 +40,27 @@ def main():
 
 #  Define the index route: GET 
 
-@application.route("/echo/<name>")
-def echo(name):
-    print(f"This was placed in the url: new-{name}")
-    val = {"new-name": name}
-    return jsonify(val)
-
 @application.route('/index.html', methods=['GET'])
 def index():
+    
+    # Get weather data (geopandas dataframe)
+    weather_df =  get_weather_data()
+
+    if weather_df is None:
+        return redirect('/dataerror.html')
+
+    # Create the map
+
+    map_html = make_weather_map(weather_df)
     #print("in maps")
-    map_html = make_map()
+    #map_html = make_map()
     if map_html is None:
         print("map not saved")
         return redirect('/maperror.html')
     application.vars['map_html'] = map_html  
     # get the current time in UTC (constant reference timezone)
     
-    application.vars['Title_line1'] = 'Current U.S. Weather Statements'
+    application.vars['Title_line1'] = 'Current U.S. Weather Statements - 2nd'
     timestamp = dt.datetime.now(dt.timezone.utc).isoformat(timespec='minutes')
     print(timestamp)
     application.vars['Title_line2'] = timestamp[0:10]+' '+timestamp[11:16]+' UTC'
@@ -64,6 +71,7 @@ def index():
 @application.route('/maps/map.html')
 @nocache
 def show_map():
+    # return the map "string" to the display templateß
     print("show map")
     return render_template_string(application.vars['map_html'])
   
@@ -71,16 +79,35 @@ def show_map():
 
 @application.route('/get_logo')
 def get_logo():
-  #print("in get logo")
-  #logo_path = os.path.join(server.root_path, 'static/img/logo.png' )
+
+    # return the logo png to the browser
     logo_path = application.vars.get("logo_path")
-  #logo_file = Path(logo_path)
-  #print(logo_path)
     if os.path.exists(logo_path):
         return send_file(logo_path)
     else:
         return render_template('error.html', culprit='logo file', details="the logo file couldn't be loaded")
+
       
+@application.route('/error.html')
+def error():
+    details = "There was some kind of error."
+    return render_template('error.html', culprit='logic', details=details)
+
+@application.route('/apierror.html')
+def apierror():
+    details = "There was an error with one of the API calls you attempted."
+    return render_template('error.html', culprit='API', details=details)
+
+@application.route('/maperror.html')
+def maperror():
+    details = "Map not found."
+    return render_template('error.html', culprit='the Map', details=details)
+
+@application.route('/dataerror.html')
+def dataerror():
+    details = "Data not available or timed out while fetching."
+    return render_template('error.html', culprit='the Map', details=details)
+
 
 
 if __name__ == "__main__":
